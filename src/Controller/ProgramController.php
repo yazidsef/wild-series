6807476e70ee512a0a1e0bf1b222f7b07899cb3f
yazid sefsaf <yazidsefsaf45@yahoo.com>
2024-07-20 20:14:19 +0200

    ajout des fixtures , fakerphp et methode show dans ProgramController

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,13 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program', name: 'app_program')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'controller_name' => 'ProgramController',
+        ]);
+    }
+    #[Route('/{id}',name:'new' , requirements: ['id' => '\d+'])]
+    public function show(int $id):Response
+    {
+        return $this->render('program/list.html.twig',[
+            'id'=>$id 
         ]);
     }
 }
