46c3a9b17b8a77fb9bdc073eb6b4f08444b97d9e
yazid sefsaf <yazidsefsaf45@yahoo.com>
2024-07-20 22:57:07 +0200

    ajout des données par fixtures

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -8,11 +8,27 @@ use Doctrine\Persistence\ObjectManager;
 
 class CategoryFixtures extends Fixture
 {
+    const   CATEGORIES = [
+        'Action',
+        'Aventure',
+        'Animation',
+        'Comédie',
+        'Drame',
+        'Fantastique',
+        'Horreur',
+        'Policier',
+        'Science-Fiction',
+        'Thriller',
+    ];
     public function load(ObjectManager $manager): void
     {
-        $category = new Category();
-        $category->setName('Horreur');
-        $manager->persist($category);
+        foreach(self::CATEGORIES as $i=>$name){
+            $category = new Category();
+            $category->setName($name);
+            $this->addReference('category_'.$i, $category);
+            $manager->persist($category);
+        }
+        
         $manager->flush();
     }
 }
--- src/DataFixtures/ProgramFixtures.php
+++ src/DataFixtures/ProgramFixtures.php
@@ -2,16 +2,30 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Program;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-
+use Faker\Factory;
 class ProgramFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+        for($i=0 ; $i<100 ; $i++){
+            $program = new Program();
+            $program->setTitle($faker->sentence);
+            $program->setSynopsis($faker->text);
+            $program->setPoster($faker->name);
+            $program->setCategory($this->getReference('category_'.rand(0,9)));
+            $program->setCountry($faker->country);
+            $manager->persist($program);
+        }
+        
 
         $manager->flush();
+    }
+    public function getDependencies()
+    {
+        return [CategoryFixtures::class];
     }
 }
