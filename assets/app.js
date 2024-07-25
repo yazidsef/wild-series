874e24eeca27e03e220265cb8e36393c8f76fc05
yazid sefsaf <yazidsefsaf45@yahoo.com>
2024-07-25 18:40:49 +0200

    ajout d'image , modification de la page index , ajout des modifications css

--- assets/app.js
+++ assets/app.js
@@ -8,23 +8,7 @@
 
 
 // Initialiser Swiper
-import Swiper, { Navigation, Pagination } from 'swiper';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 
-const swiper = new Swiper('.swiper', {
-    modules: [Navigation, Pagination],
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    loop: true, // Option to loop slides
-});
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 import './styles/navbar.css';
