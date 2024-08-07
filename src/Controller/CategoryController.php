61add93afa876f99df3c3ed92977440ccc1e8948
yazid sefsaf <yazidsefsaf45@yahoo.com>
2024-08-07 19:36:12 +0200

    adding parameters on the controller

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,17 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
+#[Route('/category', name: 'category_')]
+
 class CategoryController extends AbstractController
 {
-    #[Route('/category', name: 'app_category')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('category/index.html.twig', [
             'controller_name' => 'CategoryController',
+        ]);
+    }
+
+    #[Route('/new',name:'new')]
+    public function new(Request $request ,EntityManagerInterface $manager): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($category);
+            $manager->flush();
+            $this->redirectToRoute('category_index');
+        }
+        return $this->render('category/new.html.twig', [
+            'form'=>$form
         ]);
     }
 }
