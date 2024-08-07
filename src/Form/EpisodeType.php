238e180ab15497baf8925a3f3bace152493d1306
yazid sefsaf <yazidsefsaf45@yahoo.com>
2024-08-07 20:30:10 +0200

    adding the Episode and Season forms , controllers , views (crud)

---
+++ src/Form/EpisodeType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Episode;
+use App\Entity\Season;
+use App\Entity\WatchList;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EpisodeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('number')
+            ->add('synonpsis')
+            ->add('season', EntityType::class, [
+                'class' => Season::class,
+                'choice_label' => 'id',
+            ])
+            ->add('watchList', EntityType::class, [
+                'class' => WatchList::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Episode::class,
+        ]);
+    }
+}
