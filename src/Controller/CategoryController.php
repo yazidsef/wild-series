9552a4a94bb621bf248fe764c0c966aa6318b9b9
yazid sefsaf <yazidsefsaf45@yahoo.com>
2024-08-11 01:19:23 -0700

    deleting versions

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -9,7 +9,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use vichUploader/server/composer;
 
 #[Route('/category', name: 'category_')]
 
