d4e034ea7f469d594c3b9ea2e4a83dc3ca06921b
yazid sefsaf <yazidsefsaf45@yahoo.com>
2024-08-12 10:34:04 -0700

    changing the root of login ,

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,9 +7,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+#[Route(path: 'account/', name: 'app_login')]
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login')]
+    #[Route(path: 'login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // get the login error if there is one
