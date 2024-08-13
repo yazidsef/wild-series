52f076ceab7e2ce5e2a16fb66a8140539389b491
yazid sefsaf <yazidsefsaf45@yahoo.com>
2024-08-12 17:29:48 -0700

    ajout de formulaire commentaire , ajout de controlleur Comment

---
+++ src/Form/CommentType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comment;
+use App\Entity\Episode;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('comment')
+            ->add('rate')
+            ->add('episode', EntityType::class, [
+                'class' => Episode::class,
+'choice_label' => 'id',
+            ])
+            ->add('author', EntityType::class, [
+                'class' => User::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
