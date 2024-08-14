ddaefecd1a3fb68d0f2022dd567c7458d2060885
yazid sefsaf <yazidsefsaf45@yahoo.com>
2024-08-13 21:55:09 -0700

    adding entityManagerInterface

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
