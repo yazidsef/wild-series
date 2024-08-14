ed689dd2581cfb1ac41853f8298911e11545b380
yazid sefsaf <yazidsefsaf45@yahoo.com>
2024-08-13 20:29:17 -0700

    adding the delete function to the comments

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,32 +2,39 @@
 
 namespace App\Controller;
 
-use App\Form\CommentType;
+use App\Entity\Comment;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/comment', name: 'comment_')]
 class CommentController extends AbstractController
 {
-    #[Route('/comment', name: 'app_comment')]
+    #[Route('/comment', name: 'index')]
     public function index(Request $request , EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(CommentType::class);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $comment = $form->getData();
-            $comment->setAuthor($this->getUser());
-
-            $entityManager->persist($comment);
-            $entityManager->flush();
-
-            // Redirect or return a response
-        }
+      
 
         return $this->render('comment/index.html.twig', [
-            'form' => $form->createView(),
+            'form' => '$form->createView()'
+                ]);
+    }
+    #[Route('/delete/{id}', name:'delete')]
+    public function delete( Comment $comment,EntityManagerInterface $entityManager): Response
+    {
+        $episode=$comment->getEpisode();
+        $season = $episode ->getSeason();
+        $program = $season->getProgram();
+        $entityManager->remove($comment);
+        $entityManager->flush();
+        $this->addFlash('danger','le commentaire a été bien supprimé ');
+        
+        return $this->redirectToRoute('program_episode_show',[
+            'program'=>$program->getSlug(),
+            'season'=> $season->getId(),
+            'slug'=>$episode->getSlug()
         ]);
     }
 }
